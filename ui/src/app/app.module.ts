b6a1c59efe39b6cdd6cca3796c4a5c45b23f6784
david <david.mackintosh@monash.edu>
2017-11-08 11:03:01 +1100

    added ui

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
+import { ApiService, UserService, FooterComponent, HeaderComponent, JwtService, SharedModule } from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AuthModule,
+    HomeModule,
+    rootRouting,
+    SharedModule
+  ],
+  providers: [
+    ApiService,
+    UserService,
+    JwtService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ ui/src/app/shared/index.ts
@@ -0,0 +1,6 @@
+export * from './layout';
+export * from './models';
+export * from './services';
+export * from './shared.module';
+export * from './list-errors.component';
+export * from './show-authed.directive';
